0481b625a15bbe3e97819bb733095c6df020fbae
LimGeon <lgun202@naver.com>
2020-08-28 00:47:57 +0900

    [ADD] start screen added

--- App.js
+++ App.js
@@ -27,27 +27,28 @@ import {
 
 const App: () => React$Node = () => {
   return (
-    <View style = {styles.container}>
-      <Text style = {styles.textcolor}>팀프앙 home</Text>
-      <Image 
-      style={styles.logo}
-      source = {{url:'https://user-images.githubusercontent.com/37579661/91409009-bf86e200-e87f-11ea-9be5-75635b4f7b2a.png',}} />
+    <View style={styles.container}>
+      <Image style={{
+        aspectRatio: 0.22,
+        resizeMode: 'contain'
+      }}
+        source={require('./assets/logo.png')} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   logo: {
-      width: 250,
-      height: 90,
+    width: 300,
+    height: 200,
   },
   container: {
-    flex : 1,
-    justifyContent : 'center', //세로에서 센터
-    alignItems : 'center' //가로에서 센터 
+    flex: 1,
+    justifyContent: 'center', //세로에서 센터
+    alignItems: 'center' //가로에서 센터 
   },
   textcolor: {
-    color : 'blue',
+    color: 'blue',
   }
 });
 
